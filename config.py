e55d90329ef7a3199092c305307b19bfde8f7707
icefrog1314 <icefrog1314@163.com>
2017-10-15 22:37:43 +0800

    创建数据模型，CRUD操作，关系映射

--- config.py
+++ config.py
@@ -3,6 +3,9 @@
 环境配置文件
 """
 
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
@@ -15,3 +18,5 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(BASE_DIR,'dev.sqlite')
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 使用Flask Script
 """
 from flask_script import  Manager,Server
-from MasteringFlask import app
+from MasteringFlask import app,db,User,Post,Tag
 
 manager = Manager(app)
 
@@ -11,7 +11,7 @@ manager.add_command("server",Server())
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app,db=db,User=User,Post=Post,Tag=Tag)
 
 if __name__ == "__main__":
     manager.run()
